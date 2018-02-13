9ded75d502de01670c9156ca096eba938eba62dc
Benjamin Weidig <ben@netzgut.net>
2018-02-13 19:42:30 +0100

    Bumped version (1.2.0)

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ var (
 
 // RootCmd is the only command, so this is Tortuga
 var RootCmd = &cobra.Command{
-	Version: "1.1.0",
+	Version: "1.2.0",
 	Use:     "tt",
 	Short:   "Tortuga",
 	Args:    cobra.MaximumNArgs(1),
