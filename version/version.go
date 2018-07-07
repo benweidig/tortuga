f1d7b7d0b817f6eb2e7c8d3c71b88118c6acd1f5
Benjamin Weidig <ben@netzgut.net>
2018-07-07 14:10:38 +0200

    bumped version to 2.0.0

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Version of the app
-	Version = "1.3.1"
+	Version = "2.0.0"
 
 	// CommitHash is the commit this version was built on, needs to be set by the linker
 	CommitHash = "n/a"
