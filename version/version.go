b89e318c38dd3ceb04742786d3be1bd5ed88d4d4
Ben Weidig <ben@netzgut.net>
2026-05-24 11:49:46 +0200

    v3.0.2

--- version/version.go
+++ version/version.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	// Version of the app
-	Version = "3.0.1"
+	Version = "3.0.2"
 
 	// CommitHash is the commit this version was built on, needs to be set by the linker
 	CommitHash = "dev"
